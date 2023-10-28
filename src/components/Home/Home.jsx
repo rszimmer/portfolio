01d6d26bde9b5b0c7daec6414f424cb494bd13c2
rszimmer <rszimmer18@gmail.com>
2023-10-28 17:57:43 -0300

    :constrcution: feat - Home section static version. Header text + introduction

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import "./Home.css";
+import icon from "../../assets/Icon.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section id='home'>
+      <h1 id='heading_intro'>
+        Developing <br />
+        <span className='heading_boxes' id='dynamic_box'>
+          dynamic
+        </span>
+        <br />
+        <span className='heading_boxes' id='performatic_box'>
+          performatic
+        </span>
+        <br />
+        <span className='heading_boxes' id='accessible_box'>
+          accessible
+        </span>
+        <br />
+        front-ends
+      </h1>
+      <div id='intro_section'>
+        <p className='intro_name'>Zimmer</p>
+        <p className='intro_name'>Raquel</p>
+        <img src={icon} alt='Raquel Zimmer Photo' />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
